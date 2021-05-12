2db9d12a615fbe72a66c16eca9eea0fb83352943
kandarp dangi <kkandarpk7@gmail.com>
2021-05-12 18:20:09 +0530

    init

---
+++ Components/Sidebar.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+function Sidebar() {
+  return <Container> hello</Container>
+}
+
+const Container = styled.div`
+  color: red;
+  background: #000;
+`
+
+export default Sidebar
